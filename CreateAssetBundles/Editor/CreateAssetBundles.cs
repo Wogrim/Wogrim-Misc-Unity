e44c7e0c1e20ff88f69a89f072a79f225cd2dfdb
Tobias Carothers <105331328+Wogrim@users.noreply.github.com>
2022-05-17 14:22:08 -0700

    added CreateAssetBundles and DeleteExtraBones
    created these scripts 6 months ago, just now putting them on github with very minor changes

---
+++ CreateAssetBundles/Editor/CreateAssetBundles.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using UnityEditor;
+
+//for usage, see https://github.com/Wogrim/Wogrim-Misc-Unity
+
+public class CreateAssetBundles
+{
+    [MenuItem("Assets/Build Asset Bundles")]
+    static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = "Assets/_Asset Bundles";
+        if(!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneWindows64);
+    }
+}
